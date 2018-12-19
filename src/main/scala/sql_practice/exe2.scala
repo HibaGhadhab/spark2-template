38e38d86329292c1c3be21665ef4002ed244cb7d
HibaGhadhab <hibaghadhab@ensg.eu>
2018-12-19 09:34:25 +0100

    exe2 better version

--- src/main/scala/sql_practice/exe2.scala
+++ src/main/scala/sql_practice/exe2.scala
@@ -16,6 +16,7 @@ object exe2 {
       $"_c1".as("description"),
       $"_c2".as("nbremploye7"),
       $"_c3".as("salary7"))
+      .filter ($"code" =!= "00-0000")
     sample07DF.show
 
     val sample08DF = spark.read
@@ -25,24 +26,25 @@ object exe2 {
       $"_c1".as("description8"),
       $"_c2".as("nbremploye8"),
       $"_c3".as("salary8"))
+      .filter ($"code" =!= "00-0000")
     sample08DF.show
 
     //question 1: salaries over 100 000
     sample07DF
       .orderBy($"salary7".desc)
-      .filter($"salary7" > 100000)
+      .filter($"salary7" > 100000 && $"code" =!= "00-0000")
       .show
 
     sample08DF
       .orderBy($"salary8".desc)
-      .filter($"salary8" > 100000)
+      .filter($"salary8" > 100000 && $"code" =!= "00-0000")
       .show
 
     //question 2: growth
     sample07DF
       .join(sample08DF, Seq("code"))
       .select($"description",$"salary7", $"salary8", ((($"salary8"-$"salary7")/$"salary7")*100).as("growth"))
-      .filter($"salary8">$"salary7")
+      .filter($"salary8">$"salary7" && $"code" =!= "00-0000")
       .orderBy($"growth".desc)
       .show
 
@@ -50,7 +52,7 @@ object exe2 {
     sample07DF
       .join(sample08DF, Seq("code"))
       .select($"description",$"nbremploye7", $"nbremploye8", ($"nbremploye8"-$"nbremploye7").as("lostJobs"))
-      .filter($"nbremploye8">$"nbremploye7")
+      .filter($"nbremploye8">$"nbremploye7" && $"code" =!= "00-0000")
       .orderBy($"lostJobs".desc)
       .show
 
